b20c147e0086de768325433ccc63295228cc0446
Deeplygends <peeren.pro@gmail.com>
2019-03-19 11:25:28 +0100

    SearchController SearchView SearchViewModel && UnitTest

---
+++ Patronus.Tests/Controllers/SearchControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Patronus.Controllers;
+using Patronus.Models;
+
+namespace Patronus.Tests.Controllers
+{
+    [TestClass]
+    public class SearchControllerTest
+    {
+        [TestMethod]
+        public void TestSearchFullName()
+        {
+            SearchController controller = new SearchController();
+
+            ViewResult result = controller.Search("Tarzan") as ViewResult;
+            Assert.IsNotNull(result);
+      
+            SearchViewModel model = result.Model as SearchViewModel;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(model.Oeuvres.First().Label, "Tarzan");
+
+            Assert.AreEqual(model.Oeuvres.First().TypeOeuvre.LabelType, "Cinema");
+
+        }
+    }
+}
