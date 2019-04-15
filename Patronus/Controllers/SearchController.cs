a14c331e0b70ae00a79bc68db3298108cd188b09
DAYAUX <guillaume.dayaux@gmail.com>
2019-04-15 09:20:16 +0200

    modif vue searchpattern

--- Patronus/Controllers/SearchController.cs
+++ Patronus/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
 ﻿using Patronus.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
@@ -19,8 +20,25 @@ namespace Patronus.Controllers
         [HttpPost]
         public ActionResult SearchPattern(SearchViewModel model)
         {
-            
-            model.Oeuvres = db.Oeuvres.Where(x => x.Label.Contains(model.SearchChain)).ToList();
+            //model.Oeuvres = db.Oeuvres.Where(x => x.Label.Contains(model.SearchChainOeuvres)).ToList();
+
+            model.Oeuvres = db.Oeuvres.Where(x => x.Label.Contains(model.SearchChainOeuvres)).ToList();
+
+            var artisteList = db.Artistes
+                .Where(x => x.Nom.ToUpper().Contains(model.SearchChainOeuvres.ToUpper()) || x.Prenom.ToUpper().Contains(model.SearchChainOeuvres.ToUpper()))
+                .Select(x => x.IdArtiste)
+                .ToList();
+
+            var oeuvresByArtist = db.Oeuvres
+                .Where(
+                    y => db.Participes
+                    .Where(x => artisteList.Contains(x.IdArtiste))
+                    .Select(x => x.IdOeuvre)
+                    .ToList()
+                    .Contains(y.IdOeuvre)).ToList();
+
+            model.Oeuvres.AddRange(oeuvresByArtist);
+
             return View(model);
         }
 
