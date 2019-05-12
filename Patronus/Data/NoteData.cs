c17e23815372c9be0375965c9bc16cd963be51ec
DAYAUX <guillaume.dayaux@gmail.com>
2019-05-12 10:56:04 +0200

    Mean

---
+++ Patronus/Data/NoteData.cs
@@ -0,0 +1,20 @@
+﻿using Patronus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Patronus.Data
+{
+    public class NoteData
+    {
+        private PatronusDBEntities db = new PatronusDBEntities();
+        public double GetMeanNote(Oeuvre oeuvre)
+        {
+
+            return db.NoteOeuvres.Where(x => x.IdOeuvre == oeuvre.IdOeuvre).Average(x => x.Note);
+
+        }
+        
+    }
+}
